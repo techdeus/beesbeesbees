08a0cf51db3aef8f288df042193043f6455b45b0
techdeus <sullivan.marlon@gmail.com>
2018-09-24 12:02:48 -0700

    Sprint Complete

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,8 @@
 var Bee = function() {
+	Grub.call(this); // call the grub Superclass
+	this.age = 5;
+	this.color = 'yellow';
+	this.job = 'keep on growing';
 };
-
+Bee.prototype = Object.create(Grub.prototype); // create an object that delegates to the Superclass prototype
+Bee.prototype.constructor = Bee;
